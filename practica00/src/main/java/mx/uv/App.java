354fb6b743d509eea5425d25a155db2bf6264bb2
Antonio Espinoza <Antonio>
2024-03-07 15:38:01 -0600

    practica00

---
+++ practica00/src/main/java/mx/uv/App.java
@@ -0,0 +1,31 @@
+package mx.uv;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/", (request, response) -> "<h1>Bienvenidx</h1>");
+
+        get("/xml", (request, response) -> {
+            String respuesta = "<p><x><y>algo</y> <y>algo2</y></x></p>";
+            response.type("application/xml");
+            return respuesta;
+        });
+    }
+}
+
